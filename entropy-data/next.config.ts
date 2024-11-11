92b6e30df0111f9df026345b5a010e40dacfa3fa
BdNWoG <billy.gao@hotmail.com>
2024-11-10 20:57:59 -0800

    config

--- entropy-data/next.config.ts
+++ entropy-data/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("plotly.js-dist-min"); // Replace with any other client-only library
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
