f9c9385c9ec697a33659129c6b8ce20bfd4f23f9
BdNWoG <billy.gao@hotmail.com>
2024-11-10 20:31:37 -0800

    plot

--- entropy-data/components/PlotPanel.tsx
+++ entropy-data/components/PlotPanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useImperativeHandle } from "react";
-import Plotly, { Layout, Data } from "plotly.js-dist-min";
+import { Layout, Data } from "plotly.js-dist-min"; // Keep Layout and Data types for typing purposes
 import { PlotData } from "./types";
 
 interface Customization {
@@ -35,107 +35,114 @@ const PlotPanel: React.FC<PlotPanelProps> = ({ plotData, customization, plotRef 
   useImperativeHandle(plotRef, () => internalPlotRef.current as HTMLDivElement);
 
   useEffect(() => {
-    if (plotData && internalPlotRef.current) {
-      const traces: Data[] = Object.entries(plotData).map(([label, { timestamp, value }], index) => ({
-        x: timestamp,
-        y: value,
-        type: customization.chartType === "line" ? "scatter" : "bar",
-        mode: customization.chartType === "line" ? "lines" : undefined,
-        name: label,
-        fill: customization.chartType === "line" && customization.fill ? "tonexty" : undefined,
-        stackgroup: customization.stacked && customization.chartType === "line" ? "one" : undefined,
-        marker: { color: getColor(index) },
-        line: customization.chartType === "line" ? { width: 3, color: getColor(index) } : undefined,
-        fillcolor: customization.chartType === "line" && customization.fill ? getFillColor(index) : undefined,
-      }));
+    // Dynamically import Plotly only in the client-side environment
+    const loadPlotlyAndRender = async () => {
+      const Plotly = await import("plotly.js-dist-min");
 
-      const layout: Partial<Layout> = {
-        title: {
-          text: `<b>${customization.title}</b><br><sup><span style='color:#9ecff2'>${customization.subtitle}</span></sup>`,
-          font: { family: "sans-serif", size: 30, color: "white" },
-          xanchor: "center",
-          x: 0.5,
-        },
-        plot_bgcolor: "#030d1c",
-        paper_bgcolor: "#030d1c",
-        font: { size: 14, color: "white" },
-        xaxis: {
-          title: customization.xAxisTitle,
-          type: customization.xAxisType,
-          tickformat: customization.xAxisType === "date" ? "%b %Y" : undefined,
-          tickfont: { size: 14, color: "white" },
-          showline: true,
-          linewidth: 2,
-          linecolor: "#D1D5DB",
-          showspikes: false,
-          showgrid: customization.showGrid,
-          rangeslider: { visible: customization.chartType !== "bar-line" }, // Hide range slider in bar-line mode
-        },
-        yaxis: {
-          title: customization.yAxisTitle,
-          tickprefix: customization.yAxisPrefix,
-          ticksuffix: customization.yAxisSuffix,
-          showgrid: customization.showGrid,
-          gridcolor: "rgba(173, 176, 181, 0.6)",
-          griddash: "dash",
-          tickfont: { size: 14, color: "white" },
-          showline: true,
-          linewidth: 2,
-          linecolor: "white",
-          zeroline: true,
-          zerolinewidth: 2,
-          zerolinecolor: "white",
-          rangemode: "tozero",
-          autorange: customization.yAxisMax === "" ? true : false,
-          range: customization.yAxisMax !== "" ? [0, customization.yAxisMax] : undefined,
-        },
-        yaxis2: {
-          title: customization.yAxisRightTitle || "",
-          tickprefix: customization.yAxisRightPrefix || "",
-          ticksuffix: customization.yAxisRightSuffix || "",
-          overlaying: "y",
-          side: "right",
-          showline: true,
-          linewidth: 2,
-          linecolor: "white",
-          zeroline: true,
-          zerolinewidth: 2,
-          zerolinecolor: "white",
-        },
-        legend: {
-          orientation: "h",
-          yanchor: "bottom",
-          y: -0.25,
-          xanchor: "center",
-          x: 0.5,
-          font: { size: 16, color: "white" },
-        },
-        margin: { l: 100, r: 100, t: 100, b: 100 },
-        annotations: [
-          {
-            text: `${customization.source} <br>Date: ${new Date().toLocaleDateString()}`,
-            font: { size: 8, color: "white" },
-            showarrow: false,
-            xref: "paper",
-            yref: "paper",
-            x: 0.99,
-            y: -0.15,
-            xanchor: "right",
-            yanchor: "bottom",
-            bgcolor: "#1f2c56",
-            bordercolor: "white",
-            borderwidth: 1,
-            borderpad: 4,
+      if (plotData && internalPlotRef.current) {
+        const traces: Data[] = Object.entries(plotData).map(([label, { timestamp, value }], index) => ({
+          x: timestamp,
+          y: value,
+          type: customization.chartType === "line" ? "scatter" : "bar",
+          mode: customization.chartType === "line" ? "lines" : undefined,
+          name: label,
+          fill: customization.chartType === "line" && customization.fill ? "tonexty" : undefined,
+          stackgroup: customization.stacked && customization.chartType === "line" ? "one" : undefined,
+          marker: { color: getColor(index) },
+          line: customization.chartType === "line" ? { width: 3, color: getColor(index) } : undefined,
+          fillcolor: customization.chartType === "line" && customization.fill ? getFillColor(index) : undefined,
+        }));
+
+        const layout: Partial<Layout> = {
+          title: {
+            text: `<b>${customization.title}</b><br><sup><span style='color:#9ecff2'>${customization.subtitle}</span></sup>`,
+            font: { family: "sans-serif", size: 30, color: "white" },
+            xanchor: "center",
+            x: 0.5,
           },
-        ],
-        barmode: customization.stacked && customization.chartType === "bar" ? "stack" : undefined,
-      };
+          plot_bgcolor: "#030d1c",
+          paper_bgcolor: "#030d1c",
+          font: { size: 14, color: "white" },
+          xaxis: {
+            title: customization.xAxisTitle,
+            type: customization.xAxisType,
+            tickformat: customization.xAxisType === "date" ? "%b %Y" : undefined,
+            tickfont: { size: 14, color: "white" },
+            showline: true,
+            linewidth: 2,
+            linecolor: "#D1D5DB",
+            showspikes: false,
+            showgrid: customization.showGrid,
+            rangeslider: { visible: customization.chartType !== "bar-line" },
+          },
+          yaxis: {
+            title: customization.yAxisTitle,
+            tickprefix: customization.yAxisPrefix,
+            ticksuffix: customization.yAxisSuffix,
+            showgrid: customization.showGrid,
+            gridcolor: "rgba(173, 176, 181, 0.6)",
+            griddash: "dash",
+            tickfont: { size: 14, color: "white" },
+            showline: true,
+            linewidth: 2,
+            linecolor: "white",
+            zeroline: true,
+            zerolinewidth: 2,
+            zerolinecolor: "white",
+            rangemode: "tozero",
+            autorange: customization.yAxisMax === "" ? true : false,
+            range: customization.yAxisMax !== "" ? [0, customization.yAxisMax] : undefined,
+          },
+          yaxis2: {
+            title: customization.yAxisRightTitle || "",
+            tickprefix: customization.yAxisRightPrefix || "",
+            ticksuffix: customization.yAxisRightSuffix || "",
+            overlaying: "y",
+            side: "right",
+            showline: true,
+            linewidth: 2,
+            linecolor: "white",
+            zeroline: true,
+            zerolinewidth: 2,
+            zerolinecolor: "white",
+          },
+          legend: {
+            orientation: "h",
+            yanchor: "bottom",
+            y: -0.25,
+            xanchor: "center",
+            x: 0.5,
+            font: { size: 16, color: "white" },
+          },
+          margin: { l: 100, r: 100, t: 100, b: 100 },
+          annotations: [
+            {
+              text: `${customization.source} <br>Date: ${new Date().toLocaleDateString()}`,
+              font: { size: 8, color: "white" },
+              showarrow: false,
+              xref: "paper",
+              yref: "paper",
+              x: 0.99,
+              y: -0.15,
+              xanchor: "right",
+              yanchor: "bottom",
+              bgcolor: "#1f2c56",
+              bordercolor: "white",
+              borderwidth: 1,
+              borderpad: 4,
+            },
+          ],
+          barmode: customization.stacked && customization.chartType === "bar" ? "stack" : undefined,
+        };
 
-      Plotly.react(internalPlotRef.current, traces, layout);
-    } else if (plotData === null && internalPlotRef.current) {
-      Plotly.purge(internalPlotRef.current);
-      internalPlotRef.current.innerHTML = "";
-    }
+        Plotly.react(internalPlotRef.current, traces, layout);
+      } else if (plotData === null && internalPlotRef.current) {
+        Plotly.purge(internalPlotRef.current);
+        internalPlotRef.current.innerHTML = "";
+      }
+    };
+
+    loadPlotlyAndRender();
   }, [plotData, customization]);
 
   return (
@@ -143,7 +150,7 @@ const PlotPanel: React.FC<PlotPanelProps> = ({ plotData, customization, plotRef 
       {plotData != null ? (
         <div
           ref={internalPlotRef}
-          key={plotData ? "plot-present" : "plot-null"} 
+          key={plotData ? "plot-present" : "plot-null"}
           style={{ width: "100%", height: "100%" }}
         />
       ) : (
@@ -157,8 +164,8 @@ const PlotPanel: React.FC<PlotPanelProps> = ({ plotData, customization, plotRef 
 
 // Define the color palette
 const colors = [
-  "#213147", "#4a4a4a", "#0557f5", "#ff677d", "#9ecff2", "#ffcdb2", 
-  "#ffd1dc", "#e0e3d1", "#16553b", "#959595", "#9ab4e6", "#ffa060", 
+  "#213147", "#4a4a4a", "#0557f5", "#ff677d", "#9ecff2", "#ffcdb2",
+  "#ffd1dc", "#e0e3d1", "#16553b", "#959595", "#9ab4e6", "#ffa060",
   "#a172c3", "#4a6db1", "#041d7e", "#04e3c9"
 ];
 
